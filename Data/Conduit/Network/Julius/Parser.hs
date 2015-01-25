8bb5b524bd563778360eaeecd0c57a2bacde063a
haru2036 <haru2036@gmail.com>
2015-01-25 20:49:03 +0900

    added parser.hs but not yet implemented

---
+++ Data/Conduit/Network/Julius/Parser.hs
@@ -0,0 +1,7 @@
+module Data.Conduit.Network.Julius.Parser(
+) where
+import Data.Conduit.Network.Julius.Types
+import Text.XML.Light
+
+parse :: Element -> JuliusMessage
+parse = error 'not yet implemented'

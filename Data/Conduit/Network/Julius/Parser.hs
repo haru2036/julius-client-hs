04e6bd0b66351f9f27d9a592ae5c11a0a0da6324
haru2036 <haru2036@gmail.com>
2015-01-29 22:41:18 +0900

    temp

--- Data/Conduit/Network/Julius/Parser.hs
+++ Data/Conduit/Network/Julius/Parser.hs
@@ -1,10 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Data.Conduit.Network.Julius.Parser where
 import Data.Conduit.Network.Julius.Types
-import Text.XML.Light
+import Text.XML.Cursor
+import qualified Data.Text as T (Text)
 
 parse :: Element -> Either String JuliusMessage
 parse xElem = case (qName $ elName xElem) of
                 "STARTPROC" -> Right StartProc
                 "ENDPROC" -> Right EndProc
                 "STARTRECOG" -> Right StartRecog
+                "ENDRECOG" -> Right EndRecog
+                "RecogOut" -> Right $ RecogOut $ parseRecogOut xElem
                 _ -> Left "UnExpected Element"
+
+parseRecogOut :: Element -> recogOut
+parseRecogOut xElem = map parseShypo $ elContent xElem
+
+parseShypo xElem = Shypo rnk scr grm wps
+            where attribs = elAttribs xElem
+                  rnk = find attribs
+
+
